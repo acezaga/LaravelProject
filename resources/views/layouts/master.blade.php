e6d97436b37169e91557b8f0c096f3a8d3b9276e
acezaga <isaiahtaguibao@yahoo.com>
2017-01-02 12:09:38 +0800

    Posts, likes, user accounts updated. Sorry for the delayed commit because there is problem with the project and internet

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,11 +3,18 @@
     <head>
         <title>@yield('title')</title>
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ URL::to('src/css/main.css') }}"
+
     </head>
     <body>
         @include('includes.header')
         <div class="container">
             @yield('content')
         </div>
+
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="{{ URL::to('src/js/app.js') }}"></script>
     </body>
 </html>
